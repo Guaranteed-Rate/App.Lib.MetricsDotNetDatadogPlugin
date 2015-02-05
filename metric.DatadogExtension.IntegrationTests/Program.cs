e0a16ffac32c7dcd7ed6620af8feaa350df9b9b3
msiehoff <matthew.siehoff@guaranteedrate.com>
2015-02-05 10:00:02 -0600

    initial commit of vertical slice ('plugin' with DataDogReporter.cs referencing DogStatsd & metrics.net)

---
+++ metric.DatadogExtension.IntegrationTests/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using metric.DatadogPlugin;
+using metrics;
+using metrics.Core;
+
+namespace metric.DatadogExtension.IntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metrics = new Metrics();
+            //metrics.EnableDataDogReporting(5, TimeUnit.Seconds, "appdev", 8125);
+
+            var reporter = new DataDogReporter(metrics, "appdev", 8125);
+            reporter.Start(5, TimeUnit.Seconds);
+
+            CounterMetric counter = metrics.Counter("test", "HealthMetrics.Test.SimpleCounter");
+            HistogramMetric histogramMetric = metrics.Histogram("test", "HealthMetrics.Test.HistogramMetrics");
+            GaugeMetric gaugeMetric = metrics.Gauge("test", "HealthMetrics.Test.GaugeMetrics", () => GetNumberOfUsersLoggedIn);
+            var rand = new Random(1);
+
+            int runs = 0;
+            while (runs < 1000)
+            {
+                counter.Increment();
+                counter.Increment();
+                counter.Increment();
+
+                histogramMetric.Update(rand.Next(100));
+                histogramMetric.Update(rand.Next(100));
+                histogramMetric.Update(rand.Next(100));
+                histogramMetric.Update(rand.Next(100));
+                histogramMetric.Update(rand.Next(100));
+
+                Thread.Sleep(5000);
+
+                runs++;
+            }
+        }
+
+        private long GetNumberOfUsersLoggedIn()
+        {
+            var rand = new Random();
+
+            return rand.Next(2000);
+        }
+    }
+
+    //public class DatadogReporterTests
+    //{
+    //    [Test]
+    //    public void CounterTest()
+    //    {
+    //        var metrics = new Metrics();
+    //        //metrics.EnableDataDogReporting(5, TimeUnit.Seconds, "appdev", 8125);
+
+    //        CounterMetric counter = metrics.Counter("test", "HealthMetrics.Test.SimpleCounter");
+    //        HistogramMetric histogramMetric = metrics.Histogram("test", "HealthMetrics.Test.HistogramMetrics");
+    //        GaugeMetric gaugeMetric = metrics.Gauge("test", "HealthMetrics.Test.GaugeMetrics", GetNumberOfUsersLoggedIn);
+    //        var rand = new Random(1);
+
+    //        int runs = 0;
+    //        while (runs < 20)
+    //        {
+    //            counter.Increment();
+    //            counter.Increment();
+    //            counter.Increment();
+
+    //            histogramMetric.Update(rand.Next(100));
+    //            histogramMetric.Update(rand.Next(100));
+    //            histogramMetric.Update(rand.Next(100));
+    //            histogramMetric.Update(rand.Next(100));
+    //            histogramMetric.Update(rand.Next(100));
+
+    //            Thread.Sleep(5000);
+
+    //            runs++;
+    //        }
+    //    }
+
+    //    [Test]
+    //    public void TimerTest()
+    //    {
+    //        var metrics = new Metrics();
+    //        metrics.EnableDataDogReporting(5, TimeUnit.Seconds, "appdev", 8125);
+
+    //        HistogramMetric histogramMetric = metrics.Histogram("test", "HealthMetrics.Test.HistogramMetrics");
+    //        MeterMetric meter = metrics.Meter("test", "test meter", "test", TimeUnit.Seconds);
+
+    //        var timer = new TimerMetric(TimeUnit.Seconds, TimeUnit.Seconds, meter, histogramMetric, false);
+    //        for (int i = 0; i < 5; i++)
+    //        {
+    //            timer.Time(() =>
+    //            {
+    //                var randomTimer = new Random();
+    //                Thread.Sleep(randomTimer.Next(5) * 1000);
+    //            });
+    //        }
+    //    }
+
+    //    private long GetNumberOfUsersLoggedIn()
+    //    {
+    //        var rand = new Random();
+
+    //        return rand.Next(2000);
+    //    }
+    //}
+}
---
+++ metric.DatadogPlugin/DataDogReporter.cs
@@ -0,0 +1,74 @@
+﻿using metrics;
+using metrics.Core;
+using metrics.Reporting;
+using NUnit.Framework;
+using StatsdClient;
+
+namespace metric.DatadogPlugin
+{
+    public class DataDogReporter : ReporterBase
+    {
+        private readonly Metrics _metrics;
+
+        public DataDogReporter(Metrics metrics, string datadogAgentSvrName, int datadogListeningPort)
+            : base(new TextMessageWriter(), metrics)
+        {
+            _metrics = metrics;
+
+            var dogstatsdConfig = new StatsdConfig
+            {
+                StatsdServerName = datadogAgentSvrName,
+                StatsdPort = datadogListeningPort,
+            };
+
+            DogStatsd.Configure(dogstatsdConfig);
+        }
+
+        public override void Run()
+        {
+            foreach (var dictEntry in _metrics.All)
+            {
+                if (TryLogCounter(dictEntry.Key, dictEntry.Value))
+                    continue;
+
+                if (TryLogHistogram(dictEntry.Key, dictEntry.Value))
+                    continue;
+
+                if (dictEntry.Value is GaugeMetric)
+                    TryLogGauge(dictEntry.Key, (GaugeMetric)dictEntry.Value);
+            }
+        }
+
+        private bool TryLogHistogram(MetricName metricName, IMetric metric)
+        {
+            var histogramMetric = metric as HistogramMetric;
+            if (histogramMetric == null)
+                return false;
+
+            foreach (var value in histogramMetric.Values)
+            {
+                DogStatsd.Histogram(metricName.Name, value);
+            }
+
+            return true;
+        }
+
+        private bool TryLogCounter(MetricName metricName, IMetric metric)
+        {
+            var counterMetric = metric as CounterMetric;
+            if (counterMetric == null)
+                return false;
+
+            DogStatsd.Counter(metricName.Name, counterMetric.Count);
+
+            return true;
+        }
+
+        private bool TryLogGauge(MetricName metricName, GaugeMetric metric)
+        {
+            DogStatsd.Gauge(metricName.Name, metric.ValueAsString);
+
+            return true;
+        }
+    }
+}
